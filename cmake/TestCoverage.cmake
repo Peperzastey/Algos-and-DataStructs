852dba9042676eed65a2c6a6ff7e71411632c0ea
Przemyslaw Poljanski <peperzasty@gmail.com>
2019-10-01 23:25:32 +0200

    Add working coverage target
    Adds branch coverage beside line and function coverage.
    Uses lcov and genhtml to generate HTML coverage report.

--- cmake/TestCoverage.cmake
+++ cmake/TestCoverage.cmake
@@ -4,23 +4,33 @@
 #TODO
 # https://cmake.org/cmake/help/latest/manual/ctest.1.html#ctest-coverage-step
 
+#string(JOIN " " COVERAGE_EXCLUDE "\"/usr/*\"" "\"*test*.cpp\"") #since CMake version 3.12
+#set(COVERAGE_EXCLUDE "\"/usr/*\" \"*test*.cpp\"") #the space is escaped (when is sh), I don't know why
+# must be specified as a single string to not cause inserting of ';' between list elements
+# quotes must be escaped to survive into lcov call by sh
+
 # https://github.com/bilke/cmake-modules/blob/master/CodeCoverage.cmake
 add_custom_target(coverage
+    # clean after previous coverage
+    COMMAND lcov --zerocounters --directory . --quiet
     # Run tests
     #COMMAND ${Coverage_EXECUTABLE} ${Coverage_EXECUTABLE_ARGS}
-    COMMAND ctest -VV
+    COMMAND ctest --output-on-failure    
     # Capturing lcov counters and generating report
     #COMMAND ${Coverage_LCOV_ARGS} --gcov-tool ${GCOV_PATH} --directory . --capture --output-file coverage.info
-    COMMAND lcov --capture --directory . --output-file coverage.info --no-external
+    COMMAND lcov --capture --directory . --base-directory . --rc lcov_branch_coverage=1 --output-file coverage.info.total --quiet
+    #TODO add --quiet
+    #TODO add explicit --gcov-tool ${GCOV}
+    #TODO lcovrc config file instead of --rc
     #--directory ${CMAKE_BINARY_DIR} / CURRENT
-    
+    COMMAND lcov --remove coverage.info.total "\"/usr/*\"" "\"*test*.cpp\"" --rc lcov_branch_coverage=1 --output-file coverage.info --quiet #TODO ${COVERAGE_EXCLUDE}
     #TODO COMMAND ${LCOV_PATH} ${Coverage_LCOV_ARGS} --gcov-tool ${GCOV_PATH} --remove ${Coverage_NAME}.total ${COVERAGE_LCOV_EXCLUDES} --output-file ${PROJECT_BINARY_DIR}/${Coverage_NAME}.info.cleaned
-    # instead of --no-external + (CMAKE_)RUNTIME_OUTPUT_DIRECTORY
     
     #COMMAND ${GENHTML_PATH} ${Coverage_GENHTML_ARGS} -o ${Coverage_NAME} ${PROJECT_BINARY_DIR}/${Coverage_NAME}.info.cleaned
-    COMMAND genhtml coverage.info --output-directory coverage-html
+    COMMAND genhtml --legend --branch-coverage --rc lcov_branch_coverage=1 coverage.info --output-directory coverage-html --quiet
+    #TODO --demangle-cpp <-- TODO find_program(c++filt) (required for this option)
 
     WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
     #DEPENDS ${Coverage_DEPENDENCIES} #TODO tests targets (for ctest)
-    COMMENT "Processing test coverage and generating report."
+    COMMENT "Processing test coverage and generating report"
 )
